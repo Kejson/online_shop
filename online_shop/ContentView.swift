7d468927cb156db1ffbd97d1e02383fa97366bef
Kejson <kejson07@gmail.com>
2025-02-17 16:30:44 +0100

    Initial Commit

---
+++ online_shop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  online_shop
+//
+//  Created by Kamil Kopik on 17/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ online_shop/online_shopApp.swift
@@ -0,0 +1,17 @@
+//
+//  online_shopApp.swift
+//  online_shop
+//
+//  Created by Kamil Kopik on 17/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct online_shopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
